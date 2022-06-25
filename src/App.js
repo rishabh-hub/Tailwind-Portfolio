af8ff4811718e1f38596e0e4ecfd135f6a850ec2
rishabh-hub <rishabh.1056@gmail.com>
2022-06-25 19:28:14 +0530

    few changes

--- src/App.js
+++ src/App.js
@@ -4,19 +4,22 @@ import "./index.css"
 import Card from './Components/Card';
 import Header from './Components/Header';
 import Intro from './Components/Intro';
+import {BrowserRouter, Route} from "react-router-dom";
+
 
 function App() {
   return (
     <div className="bg-black w-screen h-screen">
-      <Header/>
+       <Header/>
         {/* <div className='sm:flex sm:row-auto'>
-        <Card />
-        <Card/>
-        <Card/>
-        </div> */}
+          <Card />
+          <Card/>
+          <Card/>
+        </div>  */}
 
         <Intro />
     </div>
+    
   );
 }
 
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -16,16 +16,12 @@ const Header = () => {
   }
 
   return (
-    <div className='flex basis-1/3 space-x-12 flex-auto justify-center text-2xl font-sans  bg-black '>
-        <div className='text-white px-1 my-5 opacity-50 hover:shadow-lg hover:shadow-blue-500/50 hover:opacity-100 transition ease-in-out delay-100 hover:-translate-y-1 hover:scale-110 duration-300'>
-        <button onClick={()=>{navClick("dev")}}>
+    <div className='flex basis-1/3 space-x-12 flex-auto justify-center sm:text-2xl text-xl font-sans  bg-black '>
+        <div className='text-white sm:px-3 sm:py-2 px-2 py-2 my-5 opacity-50 hover:shadow-lg hover:shadow-blue-500/50 hover:ring-2 rounded-xl hover:ring-blue-500 hover:opacity-100 transition ease-in-out delay-100 hover:-translate-y-1 hover:scale-110 duration-300' onClick={()=>{navClick("dev")}}>
         Dev mode
-        </button>
         </div>
-        <div className='text-white px-1 my-5 opacity-50 hover:opacity-100 hover:shadow-lg hover:shadow-red-500/50 transition ease-in-out delay-100 hover:-translate-y-1 hover:scale-110 duration-300'>
-        <button onClick={()=>{navClick("otaku")}}>
+        <div className='text-white sm:px-3 sm:py-2 px-2 py-2 my-5 opacity-50 hover:opacity-100 hover:shadow-lg hover:shadow-red-500/50 hover:ring-2 hover:ring-red-500 rounded-xl transition ease-in-out delay-100 hover:-translate-y-1 hover:scale-110 duration-300' onClick={()=>{navClick("otaku")}}>
         Otaku mode
-        </button>
         </div>
 
     </div>
