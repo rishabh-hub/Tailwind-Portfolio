3c80f554b5635a727860d4ee7d771ee5dbb612cb
rishabh-hub <rishabh.1056@gmail.com>
2024-06-16 17:00:30 +0530

    Made changes in names

--- src/App.js
+++ src/App.js
@@ -1,29 +1,26 @@
-
-import './App.css';
-import "./index.css"
-import Card from './Components/Card';
-import Header from './Components/Header';
-import Intro from './Components/Intro';
-import {BrowserRouter, Route,Routes} from "react-router-dom";
-import Otaku from './Components/Otaku';
-
+import "./App.css";
+import "./index.css";
+import Card from "./Components/Card";
+import Header from "./Components/Header";
+import Intro from "./Components/Intro";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Chill from "./Components/Chill";
 
 function App() {
   return (
     <div className="bg-black w-full h-screen">
-       <Header/>
-        {/* <div className='sm:flex sm:row-auto'>
+      <Header />
+      {/* <div className='sm:flex sm:row-auto'>
           <Card />
           <Card/>
           <Card/>
         </div>  */}
 
-        <Routes>
-          <Route path='/' element={<Intro/>}/>
-          <Route path='otakumode' element={<Otaku/>}/>
-        </Routes>
+      <Routes>
+        <Route path="/" element={<Intro />} />
+        <Route path="/chill" element={<Chill />} />
+      </Routes>
     </div>
-    
   );
 }
 
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,42 +1,55 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
-  
-  const selectDecide=(text)=>
-  {
-    if(text==="dev")
-    {
-      setDevButton("text-white sm:px-3 sm:py-2 px-2 py-2 mt-8 mb-4 shadow-lg shadow-blue-500/50 ring-2 rounded-xl ring-blue-500 opacity-100 scale-110 -translate-y-1  duration-300");
-      setOtakuButton("text-white sm:px-3 sm:py-2 px-2 py-2 mt-8 mb-4 opacity-50 hover:opacity-100 hover:shadow-lg hover:shadow-red-500/50 hover:ring-2 hover:ring-red-500 rounded-xl transition ease-in-out delay-100 hover:-translate-y-1 hover:scale-110 duration-300");
+  const selectDecide = (text) => {
+    if (text === "dev") {
+      setDevButton(
+        "text-white sm:px-3 sm:py-2 px-2 py-2 mt-8 mb-4 shadow-lg shadow-blue-500/50 ring-2 rounded-xl ring-blue-500 opacity-100 scale-110 -translate-y-1  duration-300"
+      );
+      setChillButton(
+        "text-white sm:px-3 sm:py-2 px-2 py-2 mt-8 mb-4 opacity-50 hover:opacity-100 hover:shadow-lg hover:shadow-red-500/50 hover:ring-2 hover:ring-red-500 rounded-xl transition ease-in-out delay-100 hover:-translate-y-1 hover:scale-110 duration-300"
+      );
     }
-    if(text==="otaku")
-    {
-      setOtakuButton("text-white sm:px-3 sm:py-2 px-2 py-2 mt-8 mb-4 shadow-lg shadow-red-500/50 ring-2 rounded-xl ring-red-500 opacity-100 scale-110 -translate-y-1  duration-300");
-      setDevButton("text-white sm:px-3 sm:py-2 px-2 py-2 mt-8 mb-4 opacity-50 hover:opacity-100 hover:shadow-lg hover:shadow-blue-500/50 hover:ring-2 hover:ring-blue-500 rounded-xl transition ease-in-out delay-100 hover:-translate-y-1 hover:scale-110 duration-300")
+    if (text === "chill") {
+      setChillButton(
+        "text-white sm:px-3 sm:py-2 px-2 py-2 mt-8 mb-4 shadow-lg shadow-red-500/50 ring-2 rounded-xl ring-red-500 opacity-100 scale-110 -translate-y-1  duration-300"
+      );
+      setDevButton(
+        "text-white sm:px-3 sm:py-2 px-2 py-2 mt-8 mb-4 opacity-50 hover:opacity-100 hover:shadow-lg hover:shadow-blue-500/50 hover:ring-2 hover:ring-blue-500 rounded-xl transition ease-in-out delay-100 hover:-translate-y-1 hover:scale-110 duration-300"
+      );
     }
-  }
+  };
 
-  const [devbutton, setDevButton] = useState("text-white sm:px-3 sm:py-2 px-2 py-2 mt-8 mb-4 shadow-lg shadow-blue-500/50 ring-2 rounded-xl ring-blue-500 opacity-100 scale-110 -translate-y-1  duration-300");
-  const [otakuButton, setOtakuButton] = useState("text-white sm:px-3 sm:py-2 px-2 py-2 mt-8 mb-4 opacity-50 hover:opacity-100 hover:shadow-lg hover:shadow-red-500/50 hover:ring-2 hover:ring-red-500 rounded-xl transition ease-in-out delay-100 hover:-translate-y-1 hover:scale-110 duration-300");
+  const [devbutton, setDevButton] = useState(
+    "text-white sm:px-3 sm:py-2 px-2 py-2 mt-8 mb-4 shadow-lg shadow-blue-500/50 ring-2 rounded-xl ring-blue-500 opacity-100 scale-110 -translate-y-1  duration-300"
+  );
+  const [chillButton, setChillButton] = useState(
+    "text-white sm:px-3 sm:py-2 px-2 py-2 mt-8 mb-4 opacity-50 hover:opacity-100 hover:shadow-lg hover:shadow-red-500/50 hover:ring-2 hover:ring-red-500 rounded-xl transition ease-in-out delay-100 hover:-translate-y-1 hover:scale-110 duration-300"
+  );
 
   return (
     <nav>
-    <div className='flex basis-1/3 space-x-12 flex-auto justify-center sm:text-2xl text-xl font-sans'>
-        <button className={devbutton} onClick={()=>{selectDecide("dev")}}>
-        <Link to="/" >
-        Dev mode
-        </Link>
+      <div className="flex basis-1/3 space-x-12 flex-auto justify-center sm:text-2xl text-xl font-sans">
+        <button
+          className={devbutton}
+          onClick={() => {
+            selectDecide("dev");
+          }}
+        >
+          <Link to="/">Dev mode</Link>
         </button>
-        <button className={otakuButton} onClick={()=>{selectDecide("otaku")}}>
-        <Link to="/otakumode">
-        Otaku mode
-        </Link>
+        <button
+          className={chillButton}
+          onClick={() => {
+            selectDecide("chill");
+          }}
+        >
+          <Link to="/chill">Chill mode</Link>
         </button>
-
-    </div>
+      </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
